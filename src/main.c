2dca24398f545cdf1b92ada52fd1dc8a0910b886
Keisuke Hirata <97794376+HareK02@users.noreply.github.com>
2023-12-12 00:44:56 +0000

    #1

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World!\n");
-    return 0;
+typedef struct {
+		int id;
+		char name[30];
+		char reason[50];
+} PersonCard;
+
+void Gate(PersonCard *personCard) {
+	printf("IDを入力してください: ");
+	scanf("%d", &personCard->id);
+	printf("氏名を入力してください: ");
+	scanf("%s", personCard->name);
+	printf("入構理由を入力してください: ");
+	scanf("%s", personCard->reason);
+}
+
+void Disp(PersonCard *personCard) {
+	printf("ID: %d\n", personCard->id);
+	printf("Name: %s\n", personCard->name);
+	printf("Reason: %s\n", personCard->reason);
+}
+
+int main(void) {
+	PersonCard personCard;
+	Gate(&personCard);
+	Disp(&personCard);
+	return 0;
 }
